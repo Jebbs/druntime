dlang/dmd

commit 26caea46d39f362b9b4457cd4c7973e2f9c36399

Add version (Windows): to Windows headers.

src/core/sys/windows/windows.d
@@ -14,8 +14,9 @@
  */
 module core.sys.windows.windows;
 
-
-extern (Windows) nothrow:
+version (Windows):
+extern (Windows)
+nothrow:
 
     alias uint ULONG;
     alias ULONG *PULONG;
@@ -140,7 +141,7 @@ else // Win32
     alias DWORD   *LPCOLORREF;
     alias WORD    ATOM;
 
-version (0)
+version (none)
 {   // Properly prototyped versions
     alias BOOL function(HWND, UINT, WPARAM, LPARAM) DLGPROC;
     alias VOID function(HWND, UINT, UINT_PTR, DWORD) TIMERPROC;

src/core/sys/windows/dbghelp.d
@@ -14,9 +14,9 @@
  */
 module core.sys.windows.dbghelp;
 
+version (Windows):
 
 import core.sys.windows.windows;
-
 
 alias CHAR TCHAR;
 

Jebbs/druntime

commit af8ee86963f929d52211bfba77f2a730f59b566d

Fix a version statement broken in 26caea46d39f362b9b4457cd4c7973e2f9c36399.

src/core/sys/windows/windows.d
@@ -141,7 +141,7 @@ else // Win32
     alias DWORD   *LPCOLORREF;
     alias WORD    ATOM;
 
-version (none)
+version (all)
 {   // Properly prototyped versions
     alias BOOL function(HWND, UINT, WPARAM, LPARAM) DLGPROC;
     alias VOID function(HWND, UINT, UINT_PTR, DWORD) TIMERPROC;